ae0873ad13219e28301fd5c672890e6d5559868f
ardasanlier <77684145+ardasanlier@users.noreply.github.com>
2021-01-20 10:31:06 +0300

    Create musteri.cs

---
+++ musteri.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Second
+{
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public int Yas { get; set; }
+        public string bossatir { get; set; }
+
+    }
+}
